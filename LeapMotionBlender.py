7a2b75ccd778080b2d2b7f2d91f48d19f7813287
catafest <catalinfest@gmail.com>
2013-03-28 10:46:38 +0200

    Create LeapMotionBlender.py

---
+++ LeapMotionBlender.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+
+import bpy
+import time
+import sys
+import os
+import LeapMotionupdate
+
+__all__ = ["run","update"]
+
+def run():
+    print("run")
+def update():
+    print("update")
+
